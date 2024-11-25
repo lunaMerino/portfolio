b5b8c729cdf8bf786dd361ce9a8fffc5f0247f24
lunaMerino <marialuna.garcia-arcicollar@a.vedrunasevillasj.es>
2024-11-25 09:37:54 +0100

    9no commit, +delete(tech, dev) checked

--- src/main/java/com/vedruna/portfolio/services/DeveloperServiceImpl.java
+++ src/main/java/com/vedruna/portfolio/services/DeveloperServiceImpl.java
@@ -1,17 +1,26 @@
 package com.vedruna.portfolio.services;
 
+
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vedruna.portfolio.dto.DeveloperDTO;
 import com.vedruna.portfolio.persistance.models.Developer;
+import com.vedruna.portfolio.persistance.models.Project;
 import com.vedruna.portfolio.persistance.repository.DeveloperRepository;
+import com.vedruna.portfolio.persistance.repository.ProjectRepository;
 
 @Service
 public class DeveloperServiceImpl implements DeveloperServiceI {
 
     @Autowired
     private DeveloperRepository developerRepo;
+
+    @Autowired
+    private ProjectRepository projectRepo;
 
 
     @Override
@@ -33,7 +42,17 @@ public class DeveloperServiceImpl implements DeveloperServiceI {
         if (!developerRepo.existsById(devId)) {
             throw new RuntimeException("Desarrollador no encontrado");
         }
+        Developer developer = developerRepo.findById(devId)
+                .orElseThrow(() -> new RuntimeException("Desarrollador no encontrado"));
 
-        developerRepo.deleteById(devId);
+        List<Project> projects = developer.getProjects();
+
+        for (Project project : projects) {
+            project.getDevelopers().remove(developer);
+            projectRepo.save(project);
+        }
+
+        developerRepo.delete(developer);
     }
+
 }
